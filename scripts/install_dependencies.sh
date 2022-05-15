f77c10e7650828b1f43332210ac3ea2f77970885
EC2 Default User <ec2-user@ip-10-10-1-248.us-west-1.compute.internal>
2022-05-15 03:35:02 +0000

    update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo amazon-linux-extras install php7.4
 sudo yum install -y httpd mariadb-server php
+# Code dep agent
+
+sudo yum update -y
+sudo yum install ruby -y
+sudo yum install wget -y
+
+wget https://aws-codedeploy-us-west-1.s3.us-west-1.amazonaws.com/latest/install
